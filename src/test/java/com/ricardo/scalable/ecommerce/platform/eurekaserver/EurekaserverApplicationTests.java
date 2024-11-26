5078864e1486b8cadbc76454687d8cd8c457fc24
RicardoRG96 <ricardoretamal10@gmail.com>
2024-11-26 07:32:04 -0300

    first commit

---
+++ src/test/java/com/ricardo/scalable/ecommerce/platform/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.scalable.ecommerce.platform.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
